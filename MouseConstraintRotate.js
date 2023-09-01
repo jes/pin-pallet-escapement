8fbcc1aa920abb974099ff455bd2dae1e5280fc8
James Stanley <james@incoherency.co.uk>
2023-09-01 20:24:56 +0100

    Add dodgy mouse rotation

---
+++ MouseConstraintRotate.js
@@ -0,0 +1,251 @@
+// derived from https://github.com/liabru/matter-js/blob/master/src/constraint/MouseConstraint.js
+// by jes
+
+var MouseConstraintRotate = {};
+
+(function() {
+    let Vertices = Matter.Vertices,
+        Sleeping = Matter.Sleeping,
+        Mouse = Matter.Mouse,
+        Events = Matter.Events,
+        Detector = Matter.Detector,
+        Constraint = Matter.Constraint,
+        Composite = Matter.Composite,
+        Common = Matter.Common,
+        Bounds = Matter.Bounds;
+
+    /**
+     * Creates a new mouse constraint.
+     * All properties have default values, and many are pre-calculated automatically based on other properties.
+     * See the properties section below for detailed information on what you can pass via the `options` object.
+     * @method create
+     * @param {engine} engine
+     * @param {} options
+     * @return {MouseConstraintRotate} A new MouseConstraintRotate
+     */
+    MouseConstraintRotate.create = function(engine, options) {
+        var mouse = (engine ? engine.mouse : null) || (options ? options.mouse : null);
+
+        if (!mouse) {
+            if (engine && engine.render && engine.render.canvas) {
+                mouse = Mouse.create(engine.render.canvas);
+            } else if (options && options.element) {
+                mouse = Mouse.create(options.element);
+            } else {
+                mouse = Mouse.create();
+                Common.warn('MouseConstraintRotate.create: options.mouse was undefined, options.element was undefined, may not function as expected');
+            }
+        }
+
+        var constraint = Constraint.create({ 
+            label: 'Mouse Constraint',
+            pointA: mouse.position,
+            pointB: { x: 0, y: 0 },
+            length: 0.01, 
+            stiffness: 0.1,
+            angularStiffness: 1,
+            render: {
+                strokeStyle: '#90EE90',
+                lineWidth: 3
+            }
+        });
+
+        var defaults = {
+            type: 'mouseConstraint',
+            mouse: mouse,
+            element: null,
+            body: null,
+            constraint: constraint,
+            collisionFilter: {
+                category: 0x0001,
+                mask: 0xFFFFFFFF,
+                group: 0
+            }
+        };
+
+        var mouseConstraint = Common.extend(defaults, options);
+
+        Events.on(engine, 'beforeUpdate', function() {
+            var allBodies = Composite.allBodies(engine.world);
+            MouseConstraintRotate.update(mouseConstraint, allBodies);
+            MouseConstraintRotate._triggerEvents(mouseConstraint);
+        });
+
+        return mouseConstraint;
+    };
+
+    /**
+     * Updates the given mouse constraint.
+     * @private
+     * @method update
+     * @param {MouseConstraintRotate} mouseConstraint
+     * @param {body[]} bodies
+     */
+    MouseConstraintRotate.update = function(mouseConstraint, bodies) {
+        var mouse = mouseConstraint.mouse,
+            constraint = mouseConstraint.constraint,
+            body = mouseConstraint.body;
+
+        if (mouse.button === 0) { // left button
+            if (!body) {
+                for (var i = 0; i < bodies.length; i++) {
+                    body = bodies[i];
+                    if (Bounds.contains(body.bounds, mouse.position) 
+                            && Detector.canCollide(body.collisionFilter, mouseConstraint.collisionFilter)) {
+                        for (var j = body.parts.length > 1 ? 1 : 0; j < body.parts.length; j++) {
+                            var part = body.parts[j];
+                            if (Vertices.contains(part.vertices, mouse.position)) {
+                                mouseConstraint.body = body;
+
+                                Events.trigger(mouseConstraint, 'startdrag', { mouse: mouse, body: body });
+
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (body) {
+                Body.setAngle(body, Vector.angle(mouse.position, body.position));
+                Body.setAngularVelocity(body, 0);
+                Sleeping.set(body, false);
+            }
+        } else {
+            constraint.bodyB = mouseConstraint.body = null;
+            constraint.pointB = null;
+
+            if (body)
+                Events.trigger(mouseConstraint, 'enddrag', { mouse: mouse, body: body });
+        }
+    };
+
+    /**
+     * Triggers mouse constraint events.
+     * @method _triggerEvents
+     * @private
+     * @param {mouse} mouseConstraint
+     */
+    MouseConstraintRotate._triggerEvents = function(mouseConstraint) {
+        var mouse = mouseConstraint.mouse,
+            mouseEvents = mouse.sourceEvents;
+
+        if (mouseEvents.mousemove)
+            Events.trigger(mouseConstraint, 'mousemove', { mouse: mouse });
+
+        if (mouseEvents.mousedown)
+            Events.trigger(mouseConstraint, 'mousedown', { mouse: mouse });
+
+        if (mouseEvents.mouseup)
+            Events.trigger(mouseConstraint, 'mouseup', { mouse: mouse });
+
+        // reset the mouse state ready for the next step
+        Mouse.clearSourceEvents(mouse);
+    };
+
+    /*
+    *
+    *  Events Documentation
+    *
+    */
+
+    /**
+    * Fired when the mouse has moved (or a touch moves) during the last step
+    *
+    * @event mousemove
+    * @param {} event An event object
+    * @param {mouse} event.mouse The engine's mouse instance
+    * @param {} event.source The source object of the event
+    * @param {} event.name The name of the event
+    */
+
+    /**
+    * Fired when the mouse is down (or a touch has started) during the last step
+    *
+    * @event mousedown
+    * @param {} event An event object
+    * @param {mouse} event.mouse The engine's mouse instance
+    * @param {} event.source The source object of the event
+    * @param {} event.name The name of the event
+    */
+
+    /**
+    * Fired when the mouse is up (or a touch has ended) during the last step
+    *
+    * @event mouseup
+    * @param {} event An event object
+    * @param {mouse} event.mouse The engine's mouse instance
+    * @param {} event.source The source object of the event
+    * @param {} event.name The name of the event
+    */
+
+    /**
+    * Fired when the user starts dragging a body
+    *
+    * @event startdrag
+    * @param {} event An event object
+    * @param {mouse} event.mouse The engine's mouse instance
+    * @param {body} event.body The body being dragged
+    * @param {} event.source The source object of the event
+    * @param {} event.name The name of the event
+    */
+
+    /**
+    * Fired when the user ends dragging a body
+    *
+    * @event enddrag
+    * @param {} event An event object
+    * @param {mouse} event.mouse The engine's mouse instance
+    * @param {body} event.body The body that has stopped being dragged
+    * @param {} event.source The source object of the event
+    * @param {} event.name The name of the event
+    */
+
+    /*
+    *
+    *  Properties Documentation
+    *
+    */
+
+    /**
+     * A `String` denoting the type of object.
+     *
+     * @property type
+     * @type string
+     * @default "constraint"
+     * @readOnly
+     */
+
+    /**
+     * The `Mouse` instance in use. If not supplied in `MouseConstraintRotate.create`, one will be created.
+     *
+     * @property mouse
+     * @type mouse
+     * @default mouse
+     */
+
+    /**
+     * The `Body` that is currently being moved by the user, or `null` if no body.
+     *
+     * @property body
+     * @type body
+     * @default null
+     */
+
+    /**
+     * The `Constraint` object that is used to move the body during interaction.
+     *
+     * @property constraint
+     * @type constraint
+     */
+
+    /**
+     * An `Object` that specifies the collision filter properties.
+     * The collision filter allows the user to define which types of body this mouse constraint can interact with.
+     * See `body.collisionFilter` for more information.
+     *
+     * @property collisionFilter
+     * @type object
+     */
+
+})();
--- main.js
+++ main.js
@@ -8,9 +8,14 @@ let Engine = Matter.Engine,
     Composite = Matter.Composite,
     Constraint = Matter.Constraint,
     Vector = Matter.Vector,
-    Vertices = Matter.Vertices;
+    Vertices = Matter.Vertices,
+    Mouse = Matter.Mouse;
 
-let engine = Engine.create();
+let engine = Engine.create({
+    constraintIterations: 50,
+    positionIterations: 50,
+    velocityIterations: 50,
+});
 let render = Render.create({
     element: el('matter'),
     engine: engine,
@@ -19,7 +24,21 @@ let render = Render.create({
         height: 600,
     },
 });
-let runner = Runner.create();
+let runner = Runner.create({
+    delta: 1000/240,
+});
+    var mouse = Mouse.create(render.canvas),
+        mouseConstraint = MouseConstraintRotate.create(engine, {
+            mouse: mouse,
+            constraint: {
+                stiffness: 1.0,
+                render: {
+                    visible: true,
+                }
+            }
+        });
+
+    Composite.add(engine.world, mouseConstraint);
 
 let WIDTH = el('matter').clientWidth;
 let HEIGHT = el('matter').clientHeight;
@@ -31,6 +50,8 @@ Runner.run(runner, engine);
 
 let escapeWheel = null;
 let escapeWheelConstraint = null;
+let palletBody = null;
+let palletBodyConstraint = null;
 function update() {
     // TODO: don't update if nothing has changed
 
@@ -68,6 +89,8 @@ function update() {
     if (escapeWheel) {
         Composite.remove(engine.world, escapeWheel);
         Composite.remove(engine.world, escapeWheelConstraint);
+        Composite.remove(engine.world, palletBody);
+        Composite.remove(engine.world, palletBodyConstraint);
     }
 
     escapeWheel = makeEscapeWheel({
@@ -78,6 +101,7 @@ function update() {
         locklength: locklength,
         impulselength: impulselength,
     });
+    Body.setMass(escapeWheel, 0.0001);
     Body.translate(escapeWheel, {x:WIDTH/2, y:HEIGHT*2/3});
 
     escapeWheelConstraint = Constraint.create({
@@ -87,7 +111,19 @@ function update() {
     });
     Composite.add(engine.world, [escapeWheel, escapeWheelConstraint]);
 
+    palletBody = Body.create({parts: [makePallet(pallet1), makePallet(pallet2)]});
+    Body.setMass(palletBody, 0.0001);
+    Body.translate(palletBody, {x:WIDTH/2, y:escapeWheel.position.y-PX_PER_MM*pivotsep});
+
+    palletBodyConstraint = Constraint.create({
+        pointA: {x: palletBody.position.x, y: palletBody.position.y},
+        bodyB: palletBody,
+        pointB: {x:0, y:0},
+    });
+    Composite.add(engine.world, [palletBody, palletBodyConstraint]);
+
     let freeBox = Bodies.rectangle(250, 0, 30, 30);
+    Body.setMass(freeBox, 0.0001);
     Composite.add(engine.world, freeBox);
 }
 
@@ -106,7 +142,7 @@ function makeEscapeWheel(opts) {
         parts.push(makeEscapeTooth(opts, i*(360/opts.numteeth)));
     }
 
-    return Body.create({ parts: parts });
+    return Body.create({ parts: parts, friction: 0.0, frictionAir: 0.0, frictionStatic: 0.0, restitution: 0.0 });
 }
 
 // create 1 escape tooth, with centre of wheel at (0,0)
@@ -129,6 +165,16 @@ function makeEscapeTooth(opts, angle) {
     // it at the place we asked for
     let mean = Vertices.mean(Vertices.create([startpoint,lockpoint,impulsepoint]));
     return Bodies.fromVertices(mean.x, mean.y, [startpoint, lockpoint, impulsepoint]);
+}
+
+// opts:
+//  - angle
+//  - distance
+//  - diameter
+function makePallet(opts) {
+    let x = opts.distance/2 * Math.sin(opts.angle * Math.PI/180);
+    let y = opts.distance/2 * Math.cos(opts.angle * Math.PI/180);
+    return Bodies.circle(PX_PER_MM*x, PX_PER_MM*y, PX_PER_MM*(opts.diameter/2), {friction:0.0, frictionAir: 0.0, frictionStatic: 0.0, restitution: 0.0});
 }
 
 update();
